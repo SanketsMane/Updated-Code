434a712dc061ecf11d782e7548138ae9a40a6eba
Rohan <rohandesai568@gmail.com>
2026-01-13 17:09:47 +0530

    Fix(build): disable better-auth admin plugin to debug authenticator error

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,7 +3,7 @@ import { emailOTPClient } from "better-auth/client/plugins";
 import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  plugins: [emailOTPClient(), adminClient()],
+  plugins: [emailOTPClient(), /* adminClient() */],
 });
 
 export const useAuth = () => {
